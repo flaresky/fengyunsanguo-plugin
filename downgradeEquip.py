0e97ab84b10b6fca065a1f5de896f536fd66c71a
flaresky <guanyufeiyun@hawstname11375.(none)>
2012-10-30 10:41:41 +0800

    ;

--- downgradeEquip.py
+++ downgradeEquip.py
@@ -59,8 +59,8 @@ def parsearg():
     global Delay_Time, Times, Eid
     parser = argparse.ArgumentParser(description='downgrade equip')
     parser.add_argument('-d', '--delay', required=False, type=str, default='0', metavar='4:23', help='the time will delay')
-    parser.add_argument('-i', '--equip_id', required=False, type=str, default='579639', help='equip id')
-    parser.add_argument('-t', '--times', required=False, type=int, default=1, help='downgrade times')
+    parser.add_argument('-i', '--equip_id', required=False, type=str, default='919116', help='equip id')
+    parser.add_argument('-t', '--times', required=False, type=int, default=50, help='downgrade times')
     res = parser.parse_args()
     dlist = res.delay.split(':')
     if len(dlist) == 1:
