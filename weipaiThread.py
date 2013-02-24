08d53364c9da51f21b09ebf91497b8f0365279b2
flaresky <flaresky@gmail.com>
2013-02-24 13:12:12 +0000

    ;

--- weipaiThread.py
+++ weipaiThread.py
@@ -24,7 +24,7 @@ class RoundControl:
         self.repeat_count = 0
 
     def can_continue(self, level):
-        #return True
+        return True
         level = int(level)
         if level > self.last_level:
             self.repeat_count = 0
@@ -195,7 +195,7 @@ def parsearg():
     parser.add_argument('-d', '--delay', required=False, type=str, default='0', metavar='4:23', help='the time will delay to weipai')
     parser.add_argument('-t', '--type', required=False, type=str, default='pi', help='weipai type')
     parser.add_argument('-l', '--max_level', required=False, type=int, default=5, help='got max level will exit')
-    parser.add_argument('-s', '--sell_color', required=False, type=int, default=4, help='will sell all color less or equal')
+    parser.add_argument('-s', '--sell_color', required=False, type=int, default=5, help='will sell all color less or equal')
     res = parser.parse_args()
     dlist = res.delay.split(':')
     if len(dlist) == 1:
